9795ff98a8f2b3713d4fb37d9c9041d9656675c4
Elmer C. Figuracion <ecfiguracion@yahoo.com>
2017-08-22 20:57:00 +0800

    Added sections for different regions Minor revisions on colors Set graph behaviour to skip mouse scroll first time, double click to allow map scroll

--- riskanalysis/js/map.js
+++ riskanalysis/js/map.js
@@ -1,7 +1,21 @@
-var mymap = L.map('mapid').setView([13.2354, 123.7781], 16);           
+var map = L.map('mapid').setView([13.2354, 123.7781], 16);   
+map.scrollWheelZoom.disable()
+
+map.on('focus', function() { map.scrollWheelZoom.enable(); });
+map.on('blur', function() { map.scrollWheelZoom.disable(); });
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZWNmaWd1cmFjaW9uIiwiYSI6ImNqNmNwZW5ybzF0cWwzM2w3YXh2enk2dXAifQ.HLzVBNEG5-czGJjBENEc-Q'
-}).addTo(mymap);
+}).addTo(map);
+
+map.on('click', function() {
+    if (map.scrollWheelZoom.enabled()) {
+      map.scrollWheelZoom.disable();
+      }
+      else {
+      map.scrollWheelZoom.enable();
+      }
+    });
